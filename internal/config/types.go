edf774aa1403f127c69aae7fce113d71755feb06
iosh <ioshx@foxmail.com>
2025-05-13 18:15:28 +0800

    chore: updte types

--- internal/config/types.go
+++ internal/config/types.go
@@ -1,16 +1,22 @@
 package config
 
 type Config struct {
-	Version  int      `mapstructure:"version"`
+	Global   Global   `mapstructure:"global"`
 	Frontend Frontend `mapstructure:"frontend"`
 	Docker   Docker   `mapstructure:"docker"`
 	Proxy    Proxy    `mapstructure:"proxy"`
 }
 
-type Frontend struct {
-	Type    string `mapstructure:"type"`
-	Source  string `mapstructure:"source"`
+type Global struct {
+	Version int    `mapstructure:"version"`
 	Workdir string `mapstructure:"workdir"`
+}
+
+type Frontend struct {
+	Type          string `mapstructure:"type"`
+	SiriusRepo    string `mapstructure:"sirius_repo"`
+	SiriusEthRepo string `mapstructure:"sirius_eth_repo"`
+	PrebuiltRepo  string `mapstructure:"prebuilt_repo"`
 }
 
 type Docker struct {
