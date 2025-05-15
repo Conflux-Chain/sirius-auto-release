03e1f00bc5c9e7c97c5caaffe16c63656cabf189
iosh <ioshx@foxmail.com>
2025-05-15 17:24:58 +0800

    chore: update types

---
+++ internal/config/constants.go
@@ -0,0 +1,12 @@
+package config
+
+const (
+	CORE_SPACE = "core"
+	E_SPACE    = "eSpace"
+	ALL_SPACE  = "all"
+
+	FRONTEND_TYPE_PREBUILT = "prebuilt"
+	FRONTEND_TYPE_SOURCE   = "source"
+
+	CONTAINER_TYPE_DOCKER = "docker"
+)
--- internal/config/types.go
+++ internal/config/types.go
@@ -9,28 +9,39 @@ type Config struct {
 
 type Global struct {
 	Version int    `mapstructure:"version"`
+	Space   string `mapstructure:"space"`
 	Workdir string `mapstructure:"workdir"`
 }
 
 type Frontend struct {
 	Type          string `mapstructure:"type"`
-	Space         string `mapstructure:"space"`
 	SiriusRepo    string `mapstructure:"sirius_repo"`
 	SiriusEthRepo string `mapstructure:"sirius_eth_repo"`
 	PrebuiltRepo  string `mapstructure:"prebuilt_repo"`
 }
 
-type Proxy struct {
-	Enabled bool   `mapstructure:"enabled"`
-	Type    string `mapstructure:"type"`
+type ProxySpace struct {
 	Port    int    `mapstructure:"port"`
 	API_URL string `mapstructure:"api_url"`
+}
+
+type Proxy struct {
+	Enabled   bool       `mapstructure:"enabled"`
+	Type      string     `mapstructure:"type"`
+	CoreSpace ProxySpace `mapstructure:"core_space"`
+	ESpace    ProxySpace `mapstructure:"e_space"`
+}
+
+type ContainerSpace struct {
+	Port int `mapstructure:"port"`
 }
 
 type Container struct {
 	Enabled bool   `mapstructure:"enabled"`
 	Type    string `mapstructure:"type"`
 	Name    string `mapstructure:"name"`
-	Port    int    `mapstructure:"port"`
 	Tag     string `mapstructure:"tag"`
+
+	CoreSpace ContainerSpace `mapstructure:"core_space"`
+	ESpace    ContainerSpace `mapstructure:"e_space"`
 }
